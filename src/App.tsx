3291fef8e20f0361bfb9f603227efb44bf06851c
inferrinizzard <inferrinizzard@gmail.com>
2021-02-13 16:25:31 -0800

    switch to implicit grant

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
 import React from 'react';
-import SpotifyWebApi from 'spotify-web-api-js';
 import Spotify from './Spotify';
 import './App.css';
 
 import Search from './Components/Search';
 
-// redirect on click.login, catch & fetch code → fetch token, store & redirect again
 const spotify = new Spotify('104889eeeb724a9ca5efa673f527f38f');
 
 const App: React.FC = () => {
-	if (window.location.search) {
-		const params = new URLSearchParams(window.location.search);
+	if (window.location.hash) {
+		const params = new URLSearchParams(window.location.hash);
 		if (params.get('state') === sessionStorage.getItem('spoompy-state')) {
 			sessionStorage.removeItem('spoompy-state');
-			if (params.get('error')) {
-			} else {
-				const activation_code = params.get('code') ?? '';
-				spotify.getAccessToken(activation_code);
-			}
+			spotify.access_token = params.get('#access_token') ?? '';
+			sessionStorage.setItem('spoompy-access_token', spotify.access_token);
+			spotify.setAccessToken(spotify.access_token);
 		}
 	}
-	// const spotify = new SpotifyWebApi();
 
 	return (
 		<div className="App">
 			<Search></Search>
 			<button onClick={spotify.login}>login</button>
+			<button onClick={() => spotify.searchArtists('city girl').then(res => console.log(res))}>
+				test
+			</button>
 		</div>
 	);
 };
--- src/Spotify.ts
+++ src/Spotify.ts
@@ -1,99 +1,28 @@
 import SpotifyWebApi from 'spotify-web-api-js';
 
-// client_id = '104889eeeb724a9ca5efa673f527f38f'
-const randomString = (n: number) => [...Array(n)].map(() => Math.random().toString(36)[2]).join('');
-
-// src: https://stackoverflow.com/a/48161723
-async function sha256(str: string) {
-	// encode as UTF-8
-	const msgBuffer = new TextEncoder().encode(str);
-
-	// hash the message
-	const hashBuffer = await window.crypto.subtle.digest('SHA-256', msgBuffer);
-
-	// convert ArrayBuffer to Array
-	const hashArray = Array.from(new Uint8Array(hashBuffer));
-
-	// convert bytes to hex string
-	const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
-	return hashHex;
-}
-
 const hostname = 'http://localhost:3000';
-export default class Spotify {
+export default class Spotify extends SpotifyWebApi {
 	client_id: string;
-	code_verifier: string;
-	code_challenge!: string;
 	state: string;
-
 	access_token!: string;
-	refresh_token!: string;
-	timeout!: number;
 
 	constructor(client_id: string) {
+		super();
 		this.client_id = client_id;
-		this.state = randomString(30);
+		this.state = [...Array(30)].map(() => Math.random().toString(36)[2]).join('');
 		if (!sessionStorage.getItem('spoompy-state'))
 			sessionStorage.setItem('spoompy-state', this.state);
-		this.code_verifier = randomString(64);
-		if (!sessionStorage.getItem('spoompy-code_verifier'))
-			sessionStorage.setItem('spoompy-code_verifier', this.code_verifier);
 	}
 
-	login = () =>
-		sha256(this.code_verifier).then(code_challenge => {
-			this.code_challenge = code_challenge;
-			let params = new URLSearchParams({
-				client_id: this.client_id,
-				response_type: 'token',
-				redirect_uri: hostname,
-				state: this.state,
-			});
-
-			const auth_url = `https://accounts.spotify.com/authorize?${params.toString()}`;
-			window.location.replace(auth_url);
+	login = () => {
+		const params = new URLSearchParams({
+			client_id: this.client_id,
+			response_type: 'token',
+			redirect_uri: hostname,
+			state: this.state,
 		});
 
-	getAccessToken(auth_code: string) {
-		const body = {
-			client_id: this.client_id,
-			grant_type: 'authorization_code',
-			code: auth_code,
-			redirect_uri: hostname,
-			code_verifier: this.code_verifier,
-		};
-		fetch('https://accounts.spotify.com/api/token', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-			},
-			body: JSON.stringify(body),
-		}).then(this.applyToken);
-	}
-
-	refresh = () => {
-		const body = {
-			client_id: this.client_id,
-			grant_type: 'refresh_token',
-			refresh_token: this.refresh_token,
-		};
-		fetch('https://accounts.spotify.com/api/token', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-			},
-			body: JSON.stringify(body),
-		}).then(this.applyToken);
+		const auth_url = `https://accounts.spotify.com/authorize?${params.toString()}`;
+		window.location.replace(auth_url);
 	};
-
-	async applyToken(res: Response) {
-		sessionStorage.removeItem('spoompy-code_verifier');
-		if (res.status === 200 && res.body) {
-			let { access_token, expires_in, refresh_token } = await res.json();
-			this.access_token = access_token;
-			this.timeout = expires_in;
-			this.refresh_token = refresh_token;
-			setTimeout(this.refresh, expires_in * 1000);
-		}
-	}
 }
