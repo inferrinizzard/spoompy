edba759b3b888e07839870f44ca28ba129f62c43
inferrinizzard <inferrinizzard@gmail.com>
2021-06-07 13:50:00 -0700

    update login to delay before redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Spotify, { hostname, Storage, wrapObj } from './Spotify';
+import Spotify, { Storage, wrapObj } from './Spotify';
 import './App.css';
 
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
@@ -10,6 +10,7 @@ const spotify = wrapObj(new Spotify('104889eeeb724a9ca5efa673f527f38f'));
 export const SpotifyContext = React.createContext(spotify);
 
 const App: React.FC = () => {
+	const hostname = window.location.origin;
 	if (window.location.hash && window.location.href.includes('redirect')) {
 		const params = new URLSearchParams(window.location.hash);
 		if (params.get('state') === Storage.state) {
@@ -17,10 +18,10 @@ const App: React.FC = () => {
 			spotify.access_token = params.get('#access_token') ?? '';
 			Storage.assignToken(spotify.access_token);
 			spotify.setAccessToken(spotify.access_token);
-			window.location.replace(hostname);
+			setTimeout(() => window.location.replace(hostname), 3000);
 		} else if (Storage.state) {
 			Storage.removeState();
-			window.location.replace(hostname);
+			setTimeout(() => window.location.replace(hostname), 3000);
 		}
 	}
 
@@ -31,10 +32,9 @@ const App: React.FC = () => {
 					<Switch>
 						<Route path="/home">
 							{!Storage.accessToken && (
-								<button onClick={() => spotify.login(window.location.origin + '/redirect')}>
-									login
-								</button>
+								<button onClick={() => spotify.login(hostname + '/redirect')}>login</button>
 							)}
+							<button onClick={() => (Storage.removeToken(), Storage.removeState())}>Reset</button>
 						</Route>
 						<Route path="/login"></Route>
 						<Route path="/user">
@@ -45,8 +45,8 @@ const App: React.FC = () => {
 							<PlaylistExplorer />
 						</Route>
 						<Route path="/redirect">
-							{/* add redirect page and redirect programmatically after timeout */}
-							{/* <Redirect push to={'home'} /> */}
+							<div>this is the redirect page</div>
+							<div>you will be redirected in 3 seconds</div>
 						</Route>
 						<Route>
 							<Redirect push to={'home'} />
