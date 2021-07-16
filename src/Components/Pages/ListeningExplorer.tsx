5c31487297430050d2af2a9775b56dc9bfc065a8
inferrinizzard <inferrinizzard@gmail.com>
2021-07-16 16:47:03 -0700

    add basic table

--- src/Components/Pages/ListeningExplorer.tsx
+++ src/Components/Pages/ListeningExplorer.tsx
@@ -17,12 +17,27 @@ const ListeningExplorer: React.FC<ListeningExplorerProps> = () => {
 
 	return (
 		<>
-			<h3>Recently Played</h3>
+			<h1 style={{ margin: '0.5rem' }}>Recently Played</h1>
 			{recents.length && (
 				<div>
-					{recents.map(song => (
-						<div key={song.track.id + song.played_at}>{song.track.name}</div>
-					))}
+					<table>
+						<thead>
+							<th>Name</th>
+							<th>Artists</th>
+							{/* <th>Played From</th> */}
+							<th>Played At</th>
+						</thead>
+						<tbody>
+							{recents.map(song => (
+								<tr key={song.track.id + song.played_at}>
+									<td>{song.track.name}</td>
+									<td>{song.track.artists.map(({ name }) => name).join(', ')}</td>
+									{/* <td>{song.context.href}</td> */}
+									<td>{song.played_at}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
 				</div>
 			)}
 		</>
