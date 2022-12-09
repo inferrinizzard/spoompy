0d9a8aa62119bdc529f472d7d1d32fe77151dd71
inferrinizzard <inferrinizzard@gmail.com>
2022-12-09 09:31:43 -0800

    delete old node

--- node_save_playlists/src/accessToken.js
+++
@@ -1,8 +0,0 @@
-import { execSync } from 'child_process';
-const args = process.argv.slice(2);
-
-let tokenHash = Buffer.from(args.join(':')).toString('base64');
-let accessTokenCmd = `curl -X "POST" -H "Authorization: Basic ${tokenHash}" -d grant_type=client_credentials https://accounts.spotify.com/api/token`;
-let token = JSON.parse(Buffer.from(execSync(accessTokenCmd)).toString()).access_token;
-
-console.log(token);
--- node_save_playlists/test.py
+++
@@ -1,4 +0,0 @@
-# import requests
-
-# res = requests.get('https://api.spotify.com/v1/users/12121954989/playlists',)
-# print(res.json())
