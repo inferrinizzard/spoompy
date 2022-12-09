aa0a34ae9ff73f3a480716e336923fbf8758c774
inferrinizzard <inferrinizzard@gmail.com>
2022-12-09 01:00:27 -0800

    loop and archive tracks per playlist

--- node/index.ts
+++ node/index.ts
@@ -1,4 +1,7 @@
+/* eslint-disable no-loop-func */
+/// <reference types="@types/spotify-api" />
 import SpotifyWebApiNode from 'spotify-web-api-node';
+import { writeFile } from 'fs';
 
 import * as dotenv from 'dotenv';
 dotenv.config({ path: '.env.local' });
@@ -28,6 +31,41 @@ spotify
 		).then(lists => lists.reduce((arr, list) => [...arr, ...list], []));
 		allPlaylists = allPlaylists.filter(playlist => playlist.owner.id === userId);
 
-		allPlaylists.forEach(playlist => console.log(playlist.name));
-		console.log(allPlaylists.length);
+		for (const playlist of allPlaylists) {
+			await new Promise(resolve => setTimeout(resolve, 5000));
+			await spotify
+				.getPlaylistTracks(playlist.id, { limit: 1 })
+				.then(({ body }) => body.total)
+				.then(numTracks =>
+					Promise.all(
+						new Array(Math.ceil(numTracks / 20))
+							.fill(0)
+							.map((_, i) =>
+								spotify
+									.getPlaylistTracks(playlist.id, { limit: 20, offset: 20 * i })
+									.then(({ body }) => body.items)
+							)
+					)
+				)
+				.then(lists => lists.reduce((arr, list) => [...arr, ...list], []))
+				.then(tracks => {
+					const trackData = tracks.map(item => {
+						const track = item.track as SpotifyApi.TrackObjectFull;
+						return {
+							name: track.name,
+							artists: track.artists.map(artist => artist.name).join(', '),
+							album: track.album.name,
+							id: track.id,
+							time: item.added_at,
+							addedBy: item.added_by.id,
+						};
+					});
+					writeFile(
+						`archive/2022-12-09/${playlist.name.replace(/ /g, '_')}.json`,
+						JSON.stringify(trackData),
+						() => console.log(`Archived "${playlist.name}" with ${trackData.length} items`)
+					);
+				})
+				.catch(e => console.log(`Error while archiving: "${playlist.name}"`, e));
+		}
 	});
