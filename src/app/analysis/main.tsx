2ff6433b162c3ceb08f544849a4f2e7fb5b9c18b
inferrinizzard <inferrinizzard@gmail.com>
2023-05-19 23:12:39 -0700

    add basic bar chart component

--- src/app/analysis/main.tsx
+++ src/app/analysis/main.tsx
@@ -3,6 +3,7 @@
 import { type PlaylistTrackWithName } from '@/types/common';
 
 import Count from '@/components/data/Count';
+import BarChart from '@/components/charts/BarChart';
 
 export interface AnalysisMainProps {
   playlists: PlaylistTrackWithName[];
@@ -12,6 +13,15 @@ export const AnalysisMain: React.FC<AnalysisMainProps> = ({ playlists }) => {
   return (
     <div>
       <Count value={playlists.length} caption="Total Tracks" />
+      <BarChart
+        data={playlists.reduce(
+          (acc, { playlist }) => ({
+            ...acc,
+            [playlist]: (acc[playlist as keyof typeof acc] ?? 0) + 1,
+          }),
+          {}
+        )}
+      />
     </div>
   );
 };
