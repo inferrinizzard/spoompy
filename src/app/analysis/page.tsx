0e31f4c68a2a99ce8a96c8d4916f21cd416cda0d
inferrinizzard <inferrinizzard@gmail.com>
2023-05-20 08:11:15 -0700

    remove tracksWithPlaylist util

--- src/app/analysis/page.tsx
+++ src/app/analysis/page.tsx
@@ -1,16 +1,14 @@
 import { getPlaylists } from '@/api';
 
 import AnalysisMain from './main';
-import { tracksWithPlaylist } from '@/util/playlistUtils';
 
 export const Analysis = async () => {
   const playlists = getPlaylists();
-  const playlistsWithName = tracksWithPlaylist(playlists);
 
   return (
     <main>
       {'Analysis page'}
-      <AnalysisMain playlists={playlistsWithName} />
+      <AnalysisMain playlists={playlists} />
     </main>
   );
 };
--- src/util/playlistUtils.ts
+++
@@ -1,8 +0,0 @@
-import { type PlaylistTrackWithName, type PlaylistTrack } from '@/types/common';
-
-export const tracksWithPlaylist = (playlists: Record<string, PlaylistTrack[]>) =>
-  Object.entries(playlists).reduce(
-    (acc, [playlist, tracks]) =>
-      acc.concat(tracks.map(track => Object.assign(track, { playlist }))),
-    [] as PlaylistTrackWithName[]
-  );
