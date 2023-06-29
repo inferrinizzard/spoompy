c87b70c42494916b7c025491e6716f86bb1ab815
inferrinizzard <inferrinizzard@gmail.com>
2023-06-28 20:38:25 -0700

    move playlist api fns to own file

---
+++ src/api/playlist.ts
@@ -0,0 +1,20 @@
+import { readFileSync, readdirSync } from 'fs';
+
+import { type PlaylistTrackWithName, type PlaylistTrack } from '@/types/common';
+
+export const getPlaylists = () => {
+  const basePath = 'archive/2023-03-22/redux';
+
+  const files = readdirSync(basePath);
+  const playlists = files.map(
+    file => JSON.parse(readFileSync(basePath + '/' + file, 'utf-8')) as PlaylistTrack[]
+  );
+
+  return playlists.reduce(
+    (acc, playlist, i) =>
+      acc.concat(
+        playlist.map(track => Object.assign(track, { playlist: files[i].replace(/[.]\w+$/, '') }))
+      ),
+    []
+  ) as PlaylistTrackWithName[];
+};
