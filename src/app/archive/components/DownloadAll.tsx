88461a6d31c590088126a3462c6ea1339e1953c9
inferrinizzard <inferrinizzard@gmail.com>
2023-07-01 21:13:55 -0700

    add dummy download all bar

--- src/app/archive/components/DownloadAll.tsx
+++ src/app/archive/components/DownloadAll.tsx
@@ -1,5 +1,21 @@
+'use client';
+
+import { useAppSelector } from '@/redux/client';
+import { selectPlaylists } from '@/redux/slices/playlistSlice';
+
 export interface DownloadAllProps {}
 
-export const DownloadAll: React.FC<DownloadAllProps> = () => <div></div>;
+export const DownloadAll: React.FC<DownloadAllProps> = () => {
+  const playlists = useAppSelector(selectPlaylists);
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row', gap: '2rem' }}>
+      <h2>{`Total Number of Playlists: ${playlists.length}`}</h2>
+      <button>{'Download All'}</button>
+      {/* TODO: downloads */}
+      <button>{'Download Selected'}</button>
+    </div>
+  );
+};
 
 export default DownloadAll;
