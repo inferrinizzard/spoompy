65dc3786f8819418e39dca872e14a95f842c102a
inferrinizzard <inferrinizzard@gmail.com>
2023-07-01 20:54:02 -0700

    add basic download row component with file download

--- src/app/archive/components/DownloadRow.tsx
+++ src/app/archive/components/DownloadRow.tsx
@@ -1,5 +1,19 @@
-export interface DownloadRowProps {}
+'use client';
 
-export const DownloadRow: React.FC<DownloadRowProps> = () => <div></div>;
+import { download } from '../util/download';
+
+export interface DownloadRowProps {
+  playlist: SpotifyApi.PlaylistObjectSimplified;
+}
+
+export const DownloadRow: React.FC<DownloadRowProps> = ({ playlist }) => (
+  <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+    <span>
+      <h3 style={{ display: 'inline-block' }}>{playlist.name}</h3>
+      <h6 style={{ display: 'inline-block' }}>{`Num tracks: ${playlist.tracks.total}`}</h6>
+    </span>
+    <button onClick={() => download(playlist, `${playlist.name}.json`)}>{'Download'}</button>
+  </div>
+);
 
 export default DownloadRow;
--- src/app/archive/main.tsx
+++ src/app/archive/main.tsx
@@ -1,15 +1,24 @@
+'use client';
+
+import { useAppSelector } from '@/redux/client';
+import { selectPlaylists } from '@/redux/slices/playlistSlice';
+
 import DownloadAll from './components/DownloadAll';
 import DownloadRow from './components/DownloadRow';
 
 export interface ArchiveMainProps {}
 
 export const ArchiveMain: React.FC<ArchiveMainProps> = () => {
+  const playlists = useAppSelector(selectPlaylists);
+
   return (
     <div>
       <DownloadAll />
       <section>
         {/* flex */}
-        <DownloadRow />
+        {playlists.map(playlist => (
+          <DownloadRow key={playlist.id} playlist={playlist} />
+        ))}
       </section>
     </div>
   );
