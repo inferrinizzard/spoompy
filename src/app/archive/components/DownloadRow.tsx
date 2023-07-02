c63698910e945f2af4b4828e504d53fa66e872b0
inferrinizzard <inferrinizzard@gmail.com>
2023-07-01 21:05:12 -0700

    add image to download row

--- src/app/archive/components/DownloadRow.tsx
+++ src/app/archive/components/DownloadRow.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Image from 'next/image';
+
 import { download } from '../util/download';
 
 export interface DownloadRowProps {
@@ -8,9 +10,10 @@ export interface DownloadRowProps {
 
 export const DownloadRow: React.FC<DownloadRowProps> = ({ playlist }) => (
   <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-    <span>
-      <h3 style={{ display: 'inline-block' }}>{playlist.name}</h3>
-      <h6 style={{ display: 'inline-block' }}>{`Num tracks: ${playlist.tracks.total}`}</h6>
+    <span style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '2rem' }}>
+      <Image src={playlist.images[0].url} height={150} width={150} alt={playlist.name} />
+      <h3>{playlist.name}</h3>
+      <h6>{`Num tracks: ${playlist.tracks.total}`}</h6>
     </span>
     <button onClick={() => download(playlist, `${playlist.name}.json`)}>{'Download'}</button>
   </div>
