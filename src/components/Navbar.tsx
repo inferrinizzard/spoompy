65e459f3249096f59654ead45de092f36ce25e79
inferrinizzard <inferrinizzard@gmail.com>
2023-07-06 14:47:34 -0700

    add links to nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,36 @@
 'use client';
 
+import Link from 'next/link';
 import styled from 'styled-components';
 
 const StyledNav = styled.nav`
   height: 2rem;
   width: 100%;
+  margin: 0.5rem;
 
   display: flex;
+  gap: 1rem;
 `;
 
 export interface NavbarProps {}
 
 export const Navbar: React.FC<NavbarProps> = () => {
-  return <StyledNav>{/* <input /> TODO: nav items */}</StyledNav>;
+  return (
+    <StyledNav>
+      <Link href="/">
+        <h1>{'Home'}</h1>
+      </Link>
+      <Link href="/browse">
+        <h1>{'Browse'}</h1>
+      </Link>
+      <Link href="/analysis">
+        <h1>{'Analysis'}</h1>
+      </Link>
+      <Link href="/archive">
+        <h1>{'Archive'}</h1>
+      </Link>
+    </StyledNav>
+  );
 };
 
 export default Navbar;
