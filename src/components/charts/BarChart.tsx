f39fe266c6ab9c186cb1986dce92ff0acabcd708
inferrinizzard <inferrinizzard@gmail.com>
2023-07-18 07:27:24 -0700

    replace ids in charts

--- src/components/charts/BarChart.tsx
+++ src/components/charts/BarChart.tsx
@@ -1,5 +1,7 @@
 import { ResponsiveBar } from '@nivo/bar';
 
+import store from '@/redux/store';
+
 import Block from '../Block';
 
 export interface BarChartProps {
@@ -7,12 +9,15 @@ export interface BarChartProps {
 }
 
 export const BarChart: React.FC<BarChartProps> = ({ data }) => {
+  const playlists = store.getState().playlist.playlists;
+
   const chartData = Object.entries(data).map(([id, tracks]) => ({ id, tracks }));
 
   return (
     <Block height={2} width={3} style={{ color: 'black' }}>
       <ResponsiveBar
         data={chartData}
+        axisBottom={{ format: id => playlists[id].name }}
         keys={['tracks']}
         margin={{ top: 10, right: 10, bottom: 20, left: 30 }}
         padding={0.3}
--- src/components/charts/LineChart.tsx
+++ src/components/charts/LineChart.tsx
@@ -1,5 +1,7 @@
 import { type DatumValue, ResponsiveLine } from '@nivo/line';
 
+import store from '@/redux/store';
+
 import Block from '../Block';
 
 export interface LineChartProps {
@@ -7,29 +9,38 @@ export interface LineChartProps {
 }
 
 export const LineChart = ({ datasets }: LineChartProps) => {
-  const chartData = Object.entries(datasets).map(([id, data]) => ({ id, data }));
+  const playlists = store.getState().playlist.playlists;
+
+  const chartData = Object.entries(datasets).map(([id, data]) => ({
+    id,
+    label: playlists[id].name,
+    data,
+  }));
 
   return (
     <Block height={2} width={3} style={{ color: 'black' }}>
       <ResponsiveLine
         data={chartData}
-        margin={{ top: 10, right: 80, bottom: 20, left: 30 }}
+        margin={{ top: 25, right: 20, bottom: 30, left: 30 }}
         colors={{ scheme: 'nivo' }}
         enableSlices="x"
         xScale={{ type: 'time' }}
+        axisBottom={{ format: date => date }}
         // sliceTooltip={} // TODO: add date to slice tooltip
         legends={[
           {
-            anchor: 'bottom-right',
-            direction: 'column',
+            data: chartData,
+            anchor: 'top',
+            direction: 'row',
             justify: false,
-            translateX: 100,
-            translateY: 0,
+            translateX: 0,
+            translateY: -25,
             itemsSpacing: 0,
             itemDirection: 'left-to-right',
             itemWidth: 80,
             itemHeight: 20,
             itemOpacity: 0.75,
+            itemTextColor: 'white',
             symbolSize: 12,
             symbolShape: 'circle',
             symbolBorderColor: 'rgba(0, 0, 0, .5)',
