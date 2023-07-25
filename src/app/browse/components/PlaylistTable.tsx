668bd86b22cb1db5fbf4e41fb51022b56d12842f
inferrinizzard <inferrinizzard@gmail.com>
2023-07-24 21:53:09 -0700

    add sort indicators, extra column flags, padding to playlist table

--- src/app/browse/components/PlaylistTable.tsx
+++ src/app/browse/components/PlaylistTable.tsx
@@ -1,10 +1,18 @@
 'use client';
 
+import { useState } from 'react';
+import { styled } from '@kuma-ui/core';
+
 import { useAppDispatch, useAppSelector } from '@/redux/client';
 import { selectSlice, selectSort, setSort } from '@/redux/slices/browseSlice';
 import { selectAlbums, selectArtists, selectPlaylists } from '@/redux/slices/playlistSlice';
+import { formatDate } from '@/utils/dateFormat';
 
 import { type PlaylistTrackEntityWithPlaylist } from '../TabularView';
+
+const TableCell = styled('td')`
+  padding: 0.5rem 1rem;
+`;
 
 export interface PlaylistTableProps {
   tracks: PlaylistTrackEntityWithPlaylist[];
@@ -20,6 +28,11 @@ const PlaylistTable: React.FC<PlaylistTableProps> = ({ tracks }) => {
   const sort = useAppSelector(selectSort);
   const slice = useAppSelector(selectSlice);
 
+  const [flags, setFlags] = useState({
+    showId: false,
+    showUserId: false,
+  });
+
   const handleSort = (column: string) => {
     const nextSort = () => {
       if (sort?.column === column) {
@@ -34,31 +47,35 @@ const PlaylistTable: React.FC<PlaylistTableProps> = ({ tracks }) => {
     dispatch(setSort(nextSort()));
   };
 
+  const getSortIcon = (column: string) => (sort?.column === column ? (sort.asc ? '▲' : '▼') : '');
+
   const playlistSlice = tracks.slice(slice.index * slice.size, (slice.index + 1) * slice.size);
 
   return (
-    <table>
+    <table style={{ width: '100%' }}>
       <thead>
         <tr>
           <th>{'Playlist'}</th>
-          <th onClick={() => handleSort('name')}>{'Name'}</th>
-          <th onClick={() => handleSort('artists')}>{'Artists'}</th>
-          <th onClick={() => handleSort('album')}>{'Album'}</th>
-          <th onClick={() => handleSort('time')}>{'Time'}</th>
-          <th>{'ID'}</th>
-          <th>{'Added by'}</th>
+          <th onClick={() => handleSort('name')}>{'Name' + getSortIcon('name')}</th>
+          <th onClick={() => handleSort('artists')}>{'Artists' + getSortIcon('artists')}</th>
+          <th onClick={() => handleSort('album')}>{'Album' + getSortIcon('album')}</th>
+          <th onClick={() => handleSort('time')}>{'Date Added' + getSortIcon('time')}</th>
+          {flags.showId && <th>{'ID'}</th>}
+          {flags.showUserId && <th>{'Added by'}</th>}
         </tr>
       </thead>
       <tbody>
         {playlistSlice.map(track => (
           <tr key={track.playlist + track.id}>
-            <td>{playlists[track.playlist].name}</td>
-            <td>{track.name}</td>
-            <td>{track.artists.map(id => artists[id].name)}</td>
-            <td>{albums[track.album].name}</td>
-            <td>{track.added_at}</td>
-            <td>{track.id}</td>
-            <td>{track.added_by}</td>
+            <TableCell>{playlists[track.playlist].name}</TableCell>
+            <TableCell>{track.name}</TableCell>
+            <TableCell>{track.artists.map(id => artists[id].name).join(', ')}</TableCell>
+            <TableCell>{albums[track.album].name}</TableCell>
+            <TableCell style={{ whiteSpace: 'nowrap' }}>
+              {formatDate(new Date(track.added_at), 'day')}
+            </TableCell>
+            {flags.showId && <TableCell>{track.id}</TableCell>}
+            {flags.showUserId && <TableCell>{track.added_by}</TableCell>}
           </tr>
         ))}
       </tbody>
