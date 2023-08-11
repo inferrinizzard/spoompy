58c99217f50dfacefc55a7f45d76315829ddfa60
inferrinizzard <inferrinizzard@gmail.com>
2023-08-11 07:39:34 -0700

    redirect to home page if not authed

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -1,10 +1,18 @@
+import { RedirectType } from 'next/dist/client/components/redirect';
+import { redirect } from 'next/navigation';
+
 import Navbar from '@/components/Navbar';
 import ReturnButton from '@/components/ReturnButton';
 import ReduxMain from '@/redux/components/ReduxMain';
+import store from '@/redux/store';
 
 import ArchiveMain from './main';
 
 export const Archive = async () => {
+  if (!store.getState().user.isAuthed) {
+    redirect('/', RedirectType.replace);
+  }
+
   return (
     <ReduxMain>
       <Navbar />
--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -1,10 +1,18 @@
+import { redirect } from 'next/navigation';
+import { RedirectType } from 'next/dist/client/components/redirect';
+
 import Navbar from '@/components/Navbar';
 import ReturnButton from '@/components/ReturnButton';
 import ReduxMain from '@/redux/components/ReduxMain';
+import store from '@/redux/store';
 
 import BrowseMain from './main';
 
 export const Browse = async () => {
+  if (!store.getState().user.isAuthed) {
+    redirect('/', RedirectType.replace);
+  }
+
   return (
     <ReduxMain>
       <Navbar />
