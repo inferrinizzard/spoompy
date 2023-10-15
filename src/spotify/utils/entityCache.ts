378d78b33bfebb3ce633688265fb7b33b1e5cbb5
inferrinizzard <inferrinizzard@gmail.com>
2023-10-15 10:37:52 -0700

    use zipson for smaller json encoding

--- src/spotify/utils/entityCache.ts
+++ src/spotify/utils/entityCache.ts
@@ -1,14 +1,24 @@
+import { parse, stringify } from 'zipson';
+
 export class EntityCache {
   public set = <Value>(id: string, value: Value): void => {
-    localStorage.setItem(id, JSON.stringify(value));
+    localStorage.setItem(id, this.compress(value));
   };
 
   public get = <Return>(id: string): Return | null => {
     const value = localStorage.getItem(id);
     if (value) {
-      return JSON.parse(value);
+      return this.decompress(value);
     }
 
     return null;
+  };
+
+  private readonly compress = (obj: unknown): string => {
+    return stringify(obj);
+  };
+
+  private readonly decompress = <Return>(str: string): Return => {
+    return parse(str);
   };
 }
