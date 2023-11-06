562d7399d51f7bfc25e77c6cc5900a1c34ad4daf
inferrinizzard <inferrinizzard@gmail.com>
2023-11-06 07:15:38 -0800

    add generic box component

--- src/styles/primitives/index.ts
+++ src/styles/primitives/index.ts
@@ -1,7 +1,8 @@
+import Box from './Box';
 import Button from './Button';
 import Image from './Image';
 import Link from './Link';
 import Spacer from './Spacer';
 import Text from './Text';
 
-export { Button, Image, Link, Spacer, Text };
+export { Box, Button, Image, Link, Spacer, Text };
