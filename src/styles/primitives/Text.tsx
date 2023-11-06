bfe1a45e5e4dd5905af68f8f5534b1847a8a7879
inferrinizzard <inferrinizzard@gmail.com>
2023-11-05 20:34:01 -0800

    fix text as prop

--- src/styles/primitives/Text.tsx
+++ src/styles/primitives/Text.tsx
@@ -5,10 +5,9 @@ import styled from 'styled-components';
 
 type TextType = 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 
-const StyledTextCreator = (textElement: TextType) =>
-  styled(textElement)<TextProps>`
-    ${(props) => props.fontSize && `font-size: ${props.fontSize};`}
-  `;
+const StyledText = styled.div<TextProps>`
+  ${(props) => props.fontSize && `font-size: ${props.fontSize};`}
+`;
 
 export interface TextProps {
   as?: TextType;
@@ -18,10 +17,10 @@ export interface TextProps {
 
 const Text: React.FC<
   PropsWithChildren<TextProps> & ComponentPropsWithoutRef<TextType>
-> = ({ as = 'p', children, ...props }) => {
-  const StyledText = StyledTextCreator(as);
-
-  return <StyledText {...props}>{children}</StyledText>;
-};
+> = ({ as = 'p', children, ...props }) => (
+  <StyledText as={as} {...props}>
+    {children}
+  </StyledText>
+);
 
 export default Text;
