d416d3d77a12f5b9b6d035c556eec7807fc436d5
inferrinizzard <inferrinizzard@gmail.com>
2023-11-26 11:19:54 -0800

    use next_public

--- src/spotify/constants.ts
+++ src/spotify/constants.ts
@@ -10,13 +10,14 @@ export const SPOTIFY_SCOPES = [
   'user-top-read',
 ];
 
-const ENV = process.env.VERCEL_ENV;
+const ENV = process.env.NEXT_PUBLIC_VERCEL_ENV ?? process.env.VERCEL_ENV;
 
 export const HOME_URL =
   ENV === 'production'
-    ? process.env.VERCEL_URL!
+    ? process.env.NEXT_PUBLIC_VERCEL_URL ?? process.env.VERCEL_URL!
     : ENV === 'preview'
-    ? process.env.VERCEL_BRANCH_URL!
+    ? process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL ??
+      process.env.VERCEL_BRANCH_URL!
     : 'http://localhost:3000';
 export const SPOTIFY_POSTBACK_URL = `${HOME_URL}/api/login`;
 
