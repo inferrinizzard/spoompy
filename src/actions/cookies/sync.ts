e8bd26fc9bfedf3eda866e9562d885e452e4a294
inferrinizzard <inferrinizzard@gmail.com>
2024-02-23 08:22:02 -0800

    delete cookies if empty object

--- src/actions/cookies/sync.ts
+++ src/actions/cookies/sync.ts
@@ -1,19 +1,37 @@
+import { type AccessToken } from '@spotify/web-api-ts-sdk';
+
 import { SPOTIFY_AUTH_COOKIE } from '@/spotify';
 
-import { getBrowserCookieString, setBrowserCookie } from './clientCookies';
-import { getServerCookieString, setServerCookie } from './serverCookies';
+import {
+  deleteBrowserCookie,
+  getBrowserCookie,
+  setBrowserCookie,
+} from './clientCookies';
+import {
+  deleteServerCookie,
+  getServerCookie,
+  setServerCookie,
+} from './serverCookies';
 
-export const syncCookies = (): void => {
-  const serverCookie = getServerCookieString(SPOTIFY_AUTH_COOKIE);
-  const browserCookie = getBrowserCookieString(SPOTIFY_AUTH_COOKIE);
+export const syncCookies = async (): Promise<void> => {
+  const serverCookie = await getServerCookie<AccessToken>(SPOTIFY_AUTH_COOKIE);
+  const browserCookie = getBrowserCookie<AccessToken>(SPOTIFY_AUTH_COOKIE);
+
+  if (!serverCookie?.access_token) {
+    await deleteServerCookie(SPOTIFY_AUTH_COOKIE);
+  }
+
+  if (!browserCookie?.access_token) {
+    deleteBrowserCookie(SPOTIFY_AUTH_COOKIE);
+  }
 
   // sync browser cookie to server
-  if (browserCookie) {
-    setServerCookie(SPOTIFY_AUTH_COOKIE, browserCookie);
+  if (browserCookie?.access_token) {
+    await setServerCookie(SPOTIFY_AUTH_COOKIE, browserCookie);
   }
 
   // sync server cookie to browser
-  if (serverCookie && !browserCookie) {
+  if (serverCookie?.access_token && !browserCookie?.access_token) {
     setBrowserCookie(SPOTIFY_AUTH_COOKIE, serverCookie);
   }
 };
